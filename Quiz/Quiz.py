8bd6d8f2de6abf503ef2a3e647b724ed9c79cdfa
YongjinKim <dydwls0669@gmail.com>
2020-09-12 17:16:28 +0900

    image

--- Quiz/Quiz.py
+++ Quiz/Quiz.py
@@ -11,3 +11,88 @@
 # 1. 배경 : 640 * 480 (세로 가로) - background.png
 # 2. 캐릭터 : 70 * 70 - character.png
 # 3. 똥 : 70 * 70 - enemy.png 
+
+import pygame
+# 초기화
+pygame.init()
+
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Quiz")
+
+# FPS
+clock = pygame.time.Clock()
+
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
+# 이동 속도
+character_speed = 0.6
+
+# 배경이미지 불러오기
+background = pygame.image.load("C:/Users/Yongjin/pygame/Quiz/Image/background.png")
+
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("C:/Users/Yongjin/pygame/Quiz/Image/character.png")
+character_size = character.get_rect().size      # 이미지의 크기를 구해옴
+character_width = character_size[0]         # 캐릭터의 가로 크기
+character_height = character_size[1]        # 캐릭터의 세로 크기
+character_x_pos = (screen_width / 2) - (character_width / 2)    # 화면 가로의 절반 크기에 해당하는 곳에 위치
+character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
+
+running = True
+while running:
+    dt = clock.tick(60)     #프레임수
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:   
+            running = False            
+        if event.type == pygame.KEYDOWN:    
+            if event.key == pygame.K_LEFT:  
+                to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:   
+                to_x += character_speed
+            elif event.key == pygame.K_UP:      
+                to_y -= character_speed
+            elif event.key == pygame.K_DOWN:    
+                to_y += character_speed
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+    
+    # 3. 게임 캐릭터 위치 정의
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
+
+    # 가로 경계값 처리
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 세로 경계값 처리
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
+    
+     # 5. 화면에 그리기
+    screen.blit(background, (0,0))      # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터그리기
+
+    pygame.display.update()             # 게임화면을 다시 그리기!
+
+
+# 잠시 대기
+pygame.time.delay(2000)     # 2초 대기
+
+# pygame 종료
+pygame.quit()
