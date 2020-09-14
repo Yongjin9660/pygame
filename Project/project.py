7c4db2846c944fc2fa60491d4877d7308dbea71d
YongjinKim <dydwls0669@gmail.com>
2020-09-14 23:47:52 +0900

    balloon

--- Project/project.py
+++ Project/project.py
@@ -72,9 +72,9 @@ balls = []
 balls.append({
     "pos_x" : 50,   # 공의 x 좌표
     "pos_y" : 50,   # 공의 y 좌표
-    "img_dix" : 0,  # 공의 이미지 인덱스
+    "img_idx" : 0,  # 공의 이미지 인덱스
     "to_x" : 3,     # x 축 이동방향
-    "to_y" : -6     # y 축 이동방향
+    "to_y" : -6,     # y 축 이동방향
     "init_spe_y" : ball_speed_y[0]  # y 최초 속도
 })
 
@@ -113,7 +113,31 @@ while running:
     weapons = [[w[0], w[1] - weapon_speed] for w in weapons]    # 무기 위치를 위로
     
     # 천장에 닿은 무기 없애기
-    weapons = [[w[0],w[1]] for w in weapons if w[1] > 0]
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+
+    # 공 위치 정의
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+
+        # 가로벽에 닿았을 때 공 이동 위치 변경
+        if ball_pos_x <=0 or ball_pos_x > screen_width - ball_width:
+            ball_val["to_x"] = ball_val["to_x"] * -1
+
+        # 세로 위치
+        # 스테이지에 튕겨서 올라가는 처리
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spe_y"]
+        else:   # 그 외의 모든 경우에는 속도를 증가
+            ball_val["to_y"] += 0.5
+
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
 
     # 4. 충돌 처리
 
@@ -123,9 +147,14 @@ while running:
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
 
+    for idx, val in enumerate(balls):
+        ball_pos_x = val["pos_x"]
+        ball_pos_y = val["pos_y"]
+        ball_img_idx = val["img_idx"]
+        screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
-
 
     pygame.display.update()
 
