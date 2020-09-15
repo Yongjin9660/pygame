c0a76d97f284082959deb1dcdfcdc34c9091bb0b
YongjinKim <dydwls0669@gmail.com>
2020-09-15 20:30:00 +0900

    middle

--- Project/project.py
+++ Project/project.py
@@ -141,6 +141,42 @@ while running:
 
     # 4. 충돌 처리
 
+    # 캐릭터 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        # 공 rect 정보 업데이트
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        # 공과 캐릭터 충돌 처리
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+
+        # 공과 무기들 충돌 처리
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weopon_rect = weopon.get_rect()
+            weapon_x_pos = weopon_val[0]
+            weapon_y_pos = weapon_val[1]
+
+            # 무기 rect 정보 업데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_x_pos
+            weapon_rect.top = weapon_y_pos
+
+            # 충돌 체크
+            if weapon_rect.colliderect(ball_rect):
+                
+
+
     # 5. 화면에 그리기
     screen.blit(background, (0, 0))
 
